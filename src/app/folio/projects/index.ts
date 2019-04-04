747219b5c9f8575fbea005989a09319bf53438af
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-04-04 21:11:15 +0530

    updated new about page

--- src/app/folio/projects/index.ts
+++ src/app/folio/projects/index.ts
@@ -1,10 +1,4 @@
-import blk from './blk';
-import ico from './ico';
-// import kit from './kit';
-import rhc from './rhc';
-import cam from './cam';
-import web from './web';
-import scp from './scp';
+
 import cm from './cm'
 import N from './N';
 export default [
